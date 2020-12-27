e0feb2151810cf4f8f117166c96fe24e5c551702
hjm10 <55585306+hjm10@users.noreply.github.com>
2020-12-27 22:53:33 +0000

    stuff

--- company/js/nav.js
+++ company/js/nav.js
@@ -26,3 +26,18 @@ function hide(element) {
     element.classList.remove('show');
     element.classList.add('hide');
 }
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
