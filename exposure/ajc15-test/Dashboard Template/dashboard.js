e009e9647bcc173b2480247dddfe8b41c83b3704
hjm10 <55585306+hjm10@users.noreply.github.com>
2021-02-01 12:35:00 +0000

    maps dahsboard

--- exposure/ajc15-test/Dashboard Template/dashboard.js
+++ exposure/ajc15-test/Dashboard Template/dashboard.js
@@ -52,3 +52,21 @@ function toggleTab(btnId, tabId) {
         tab.addClass('show');
     }
 }
+
+/*Using heriot watt locatioin map from company website to set layout for dahsboard location map*/ 
+var key = 'AIzaSyAsSFGDF9I8ccXM0XAHWQXLpjLwH-B5sGw';
+var map;
+var hwLocation = { lat: 55.913, lng: -3.323 }
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 55.913, lng: -3.323 },
+        zoom: 13,
+    });
+
+    const marker = new google.maps.Marker({
+        position: hwLocation,
+        map: map,
+    });
+
+}
